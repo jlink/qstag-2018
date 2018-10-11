28e565f9de0de068f349dbee3816e4e97dceab56
Johannes Link <business@johanneslink.net>
2018-10-11 11:28:07 +0200

    prime factors kata done

--- src/main/java/qstag/Primes.java
+++ src/main/java/qstag/Primes.java
@@ -5,12 +5,16 @@ import java.util.*;
 public class Primes {
 	public static List<Integer> factorize(int number) {
 		List<Integer> factors = new ArrayList<>();
-		for (int prime = 2; prime <= 3; prime++) {
-			while (number % prime == 0) {
+		for (int prime = 2; prime <= number; prime++) {
+			while (isDivisibleBy(number, prime)) {
 				factors.add(prime);
 				number = number / prime;
 			}
 		}
 		return factors;
+	}
+
+	private static boolean isDivisibleBy(int number, int divisor) {
+		return number % divisor == 0;
 	}
 }
--- src/test/java/qstag/PrimeFactorizationTests.java
+++ src/test/java/qstag/PrimeFactorizationTests.java
@@ -23,9 +23,20 @@ class PrimeFactorizationTests {
 	}
 
 	@Test
-	void factorizingMultiplesOf2and3() {
+	void factorizingMixedMultiplesOf2and3() {
 		assertPrimeFactors(6, asList(2, 3));
 		assertPrimeFactors(108, asList(2, 2, 3, 3, 3));
+	}
+
+	@Test
+	void higherPrimes() {
+		assertPrimeFactors(17, asList(17));
+		assertPrimeFactors(103, asList(103));
+	}
+
+	@Test
+	void multipleOfAllPrimesUpTo13() {
+		assertPrimeFactors(30030, asList(2, 3, 5, 7, 11, 13));
 	}
 
 	private void assertPrimeFactors(int number, List<Integer> expected) {

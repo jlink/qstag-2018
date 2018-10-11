10da10d90f6f55e20b63fe5e6581e790f424850f
Johannes Link <business@johanneslink.net>
2018-10-11 09:52:52 +0200

    Added floating addition test to MyFirstTests

--- src/test/java/qstag/MyFirstTests.java
+++ src/test/java/qstag/MyFirstTests.java
@@ -10,4 +10,20 @@ class MyFirstTests {
 		Assertions.assertEquals(11, sum);
 	}
 
+	// Delete before Demo:
+	@Test
+	void floatAdditionInJava() {
+		double sum = 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		sum = sum + 0.1;
+		Assertions.assertEquals(1.0, sum, 0.01);
+	}
+
 }
